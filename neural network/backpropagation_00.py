5c043a0fdf7b627ca8bcbdfb3b2b697a8bb3bf6e
ken_xu <1786546913@qq.com>
2017-12-12 17:02:02 +0800

    add some summary

--- neural network/backpropagation_00.py
+++ neural network/backpropagation_00.py
@@ -124,6 +124,7 @@ class NeuralNetwork:
                 self.hidden_layer.neurons[h].weights[w_ih] -= self.LEARNING_RATE*pd_error_wrt_weight
 
     def calculate_total_error(self, training_sets):
+        #  使用平方差计算训练集误差
         total_error = 0
         for t in range(len(training_sets)):
             training_inputs, training_outputs = training_sets[t]
@@ -252,7 +253,7 @@ nn = NeuralNetwork(2, 2, 2, hidden_layer_weights=[0.15, 0.2, 0.25, 0.3], hidden_
                    output_layer_weights=[0.4, 0.45, 0.5, 0.55], output_layer_bias=0.6)
 for i in range(10000):
     nn.train([0.05, 0.1], [0.01, 0.99])
-    print(i, round(nn.calculate_total_error([[[0.05, 0.1], [0.01, 0.99]]]), 9))
+    print(i, round(nn.calculate_total_error([[[0.05, 0.1], [0.01, 0.99]]]), 9))  # 截断处理只保留小数点后9位
 
 # XOR example:
 
